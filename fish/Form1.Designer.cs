13cb9d36d39ba557c818fdfa0155455faea9d2c5
zhangfeifei <zhangfeifei@okwanshiwu.com>
2019-10-03 19:11:13 +0800

    initialize rep

---
+++ fish/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace fish
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.imageBox3 = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 41);
+            this.button1.Margin = new System.Windows.Forms.Padding(1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(109, 97);
+            this.imageBox1.Margin = new System.Windows.Forms.Padding(1);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(591, 363);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            this.imageBox1.Click += new System.EventHandler(this.imageBox1_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(109, 13);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(80, 80);
+            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // imageBox3
+            // 
+            this.imageBox3.Location = new System.Drawing.Point(219, 13);
+            this.imageBox3.Name = "imageBox3";
+            this.imageBox3.Size = new System.Drawing.Size(115, 80);
+            this.imageBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox3.TabIndex = 2;
+            this.imageBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(971, 516);
+            this.Controls.Add(this.imageBox3);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private Emgu.CV.UI.ImageBox imageBox3;
+    }
+}
+
---
+++ fish/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace fish
+{
+
+
+    public partial class Form1 : Form
+    {
+        [DllImport("User32.dll", EntryPoint = "FindWindow")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        #region GetWindowCapture的dll引用
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rectangle rect);
+
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr CreateCompatibleDC(
+            IntPtr hdc // handle to DC
+        );
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr CreateCompatibleBitmap(
+            IntPtr hdc,         // handle to DC
+            int nWidth,      // width of bitmap, in pixels
+            int nHeight      // height of bitmap, in pixels
+        );
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr SelectObject(
+            IntPtr hdc,           // handle to DC
+            IntPtr hgdiobj    // handle to object
+        );
+        [DllImport("gdi32.dll")]
+        private static extern int DeleteDC(
+            IntPtr hdc           // handle to DC
+        );
+        [DllImport("user32.dll")]
+        private static extern bool PrintWindow(
+            IntPtr hwnd,                // Window to copy,Handle to the window that will be copied.
+            IntPtr hdcBlt,              // HDC to print into,Handle to the device context.
+            UInt32 nFlags               // Optional flags,Specifies the drawing options. It can be one of the following values.
+        );
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetWindowDC(
+                IntPtr hwnd
+            );
+        #endregion
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            //注册热键F5，Id号为103。   
+            HotKey.RegisterHotKey(Handle, 103, HotKey.KeyModifiers.None, Keys.F11);
+        }
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_HOTKEY = 0x0312;
+            //按快捷键    
+            switch (m.Msg)
+            {
+                case WM_HOTKEY:
+                    switch (m.WParam.ToInt32())
+                    {
+
+                        case 103:
+                            button1_Click(null, null);
+                            break;
+                    }
+                    break;
+            }
+            base.WndProc(ref m);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Bitmap memory = new Bitmap(1920, 1080);
+            Graphics g = Graphics.FromImage(memory);
+            Size mySize = new Size(1920, 1080);
+            g.CopyFromScreen(0, 0, 0, 0, mySize);
+
+
+           // string lpszParentWindow = "魔兽世界"; //窗口标题
+
+           // IntPtr ParenthWnd = new IntPtr(0);
+
+            //查到窗体，得到整个窗体
+           // ParenthWnd = FindWindow(null, lpszParentWindow);
+           // var bitmap = GetWindowCapture(ParenthWnd);
+
+            Image<Bgr, byte> image = new Image<Bgr, byte>(memory);
+
+            Mat img1 = image.Mat;//CvInvoke.Imread("../fish.jpg");
+            Mat img2 = CvInvoke.Imread("../piao2.png");
+
+            imageBox3.Image = img1;
+            imageBox2.Image = img2;
+
+            Mat imgout = new Mat();
+            CvInvoke.MatchTemplate(img1, img2, imgout,TemplateMatchingType.CcoeffNormed);
+            var max_loc = new Point();
+            var min_loc = new Point();
+            double max = 0, min = 0;
+            CvInvoke.MinMaxLoc(imgout, ref min, ref max, ref min_loc, ref max_loc);
+            imageBox1.Image = img1;
+            Size img2Size = img2.Size;
+            int x = (int) (img2Size.Width * 0.5) + max_loc.X;
+            int y = (int)(img2Size.Height * 0.5) + max_loc.Y;
+
+            CvInvoke.Circle(img1, new Point(x,y), 50,new MCvScalar(255,0,0));
+            
+        }
+
+        private void imageBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public static Bitmap GetWindowCapture(IntPtr hWnd)
+        {
+            IntPtr hscrdc = GetWindowDC(hWnd);
+            Rectangle windowRect = new Rectangle();
+            GetWindowRect(hWnd, ref windowRect);
+            int width = Math.Abs(windowRect.X - windowRect.Width);
+            int height = Math.Abs(windowRect.Y - windowRect.Height);
+            IntPtr hbitmap = CreateCompatibleBitmap(hscrdc, width, height);
+            IntPtr hmemdc = CreateCompatibleDC(hscrdc);
+            SelectObject(hmemdc, hbitmap);
+            PrintWindow(hWnd, hmemdc, 0);
+            Bitmap bmp = Image.FromHbitmap(hbitmap);
+            DeleteDC(hscrdc);//删除用过的对象
+            DeleteDC(hmemdc);//删除用过的对象
+            return bmp;
+        }
+
+
+    }
+}
