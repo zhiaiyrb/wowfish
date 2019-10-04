356569f075a4d84d055760d6595f10e68c98cdf9
zhangfeifei <11062465@qq.com>
2019-10-04 16:38:56 +0800

    初步完善逻辑

--- fish/Form1.Designer.cs
+++ fish/Form1.Designer.cs
@@ -30,12 +30,9 @@
         {
             this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
-            this.imageBox1 = new Emgu.CV.UI.ImageBox();
-            this.imageBox2 = new Emgu.CV.UI.ImageBox();
-            this.imageBox3 = new Emgu.CV.UI.ImageBox();
-            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).BeginInit();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -49,34 +46,27 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // imageBox1
+            // richTextBox1
             // 
-            this.imageBox1.Location = new System.Drawing.Point(109, 97);
-            this.imageBox1.Margin = new System.Windows.Forms.Padding(1);
-            this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(591, 363);
-            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.imageBox1.TabIndex = 2;
-            this.imageBox1.TabStop = false;
-            this.imageBox1.Click += new System.EventHandler(this.imageBox1_Click);
+            this.richTextBox1.Location = new System.Drawing.Point(126, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(746, 450);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
             // 
-            // imageBox2
+            // timer1
             // 
-            this.imageBox2.Location = new System.Drawing.Point(109, 13);
-            this.imageBox2.Name = "imageBox2";
-            this.imageBox2.Size = new System.Drawing.Size(80, 80);
-            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.imageBox2.TabIndex = 2;
-            this.imageBox2.TabStop = false;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // imageBox3
+            // button2
             // 
-            this.imageBox3.Location = new System.Drawing.Point(219, 13);
-            this.imageBox3.Name = "imageBox3";
-            this.imageBox3.Size = new System.Drawing.Size(115, 80);
-            this.imageBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.imageBox3.TabIndex = 2;
-            this.imageBox3.TabStop = false;
+            this.button2.Location = new System.Drawing.Point(17, 439);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
             // Form1
             // 
@@ -84,17 +74,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(971, 516);
-            this.Controls.Add(this.imageBox3);
-            this.Controls.Add(this.imageBox2);
-            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load_1);
-            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -102,9 +88,9 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private Emgu.CV.UI.ImageBox imageBox1;
-        private Emgu.CV.UI.ImageBox imageBox2;
-        private Emgu.CV.UI.ImageBox imageBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- fish/Form1.cs
+++ fish/Form1.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 using Emgu.CV;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.CV.Util;
 
 namespace fish
 {
@@ -18,9 +20,34 @@ namespace fish
 
     public partial class Form1 : Form
     {
+        #region windows的dll引用
+        [DllImport("user32.dll", EntryPoint = "keybd_event", SetLastError = true)]
+        public static extern void keybd_event(Keys bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+
+        [System.Runtime.InteropServices.DllImport("user32")]
+        private static extern int mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        [DllImport("User32.dll")]
+        private static extern bool SetCursorPos(int x, int y);
+        //移动鼠标 
+        const int MOUSEEVENTF_MOVE = 0x0001;
+        //模拟鼠标左键按下 
+        const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        //模拟鼠标左键抬起 
+        const int MOUSEEVENTF_LEFTUP = 0x0004;
+        //模拟鼠标右键按下 
+        const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        //模拟鼠标右键抬起 
+        const int MOUSEEVENTF_RIGHTUP = 0x0010;
+        //模拟鼠标中键按下 
+        const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        //模拟鼠标中键抬起 
+        const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+        //标示是否采用绝对坐标 
+        const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+
+
         [DllImport("User32.dll", EntryPoint = "FindWindow")]
         private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-        #region GetWindowCapture的dll引用
         [DllImport("user32.dll")]
         private static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rectangle rect);
 
@@ -53,83 +80,6 @@ namespace fish
         private static extern IntPtr GetWindowDC(
                 IntPtr hwnd
             );
-        #endregion
-
-
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        private void Form1_Load_1(object sender, EventArgs e)
-        {
-            //注册热键F5，Id号为103。   
-            HotKey.RegisterHotKey(Handle, 103, HotKey.KeyModifiers.None, Keys.F11);
-        }
-        protected override void WndProc(ref Message m)
-        {
-            const int WM_HOTKEY = 0x0312;
-            //按快捷键    
-            switch (m.Msg)
-            {
-                case WM_HOTKEY:
-                    switch (m.WParam.ToInt32())
-                    {
-
-                        case 103:
-                            button1_Click(null, null);
-                            break;
-                    }
-                    break;
-            }
-            base.WndProc(ref m);
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-            Bitmap memory = new Bitmap(1920, 1080);
-            Graphics g = Graphics.FromImage(memory);
-            Size mySize = new Size(1920, 1080);
-            g.CopyFromScreen(0, 0, 0, 0, mySize);
-
-
-           // string lpszParentWindow = "魔兽世界"; //窗口标题
-
-           // IntPtr ParenthWnd = new IntPtr(0);
-
-            //查到窗体，得到整个窗体
-           // ParenthWnd = FindWindow(null, lpszParentWindow);
-           // var bitmap = GetWindowCapture(ParenthWnd);
-
-            Image<Bgr, byte> image = new Image<Bgr, byte>(memory);
-
-            Mat img1 = image.Mat;//CvInvoke.Imread("../fish.jpg");
-            Mat img2 = CvInvoke.Imread("../piao2.png");
-
-            imageBox3.Image = img1;
-            imageBox2.Image = img2;
-
-            Mat imgout = new Mat();
-            CvInvoke.MatchTemplate(img1, img2, imgout,TemplateMatchingType.CcoeffNormed);
-            var max_loc = new Point();
-            var min_loc = new Point();
-            double max = 0, min = 0;
-            CvInvoke.MinMaxLoc(imgout, ref min, ref max, ref min_loc, ref max_loc);
-            imageBox1.Image = img1;
-            Size img2Size = img2.Size;
-            int x = (int) (img2Size.Width * 0.5) + max_loc.X;
-            int y = (int)(img2Size.Height * 0.5) + max_loc.Y;
-
-            CvInvoke.Circle(img1, new Point(x,y), 50,new MCvScalar(255,0,0));
-            
-        }
-
-        private void imageBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
 
         public static Bitmap GetWindowCapture(IntPtr hWnd)
         {
@@ -148,6 +98,276 @@ namespace fish
             return bmp;
         }
 
+       
 
+        #endregion
+
+        private Mat fishFloatMat = null;
+        private readonly int screenW = Screen.PrimaryScreen.Bounds.Width;
+        private readonly int screenH = Screen.PrimaryScreen.Bounds.Height;
+
+        private bool fishStart = false;
+        public Form1()
+        {
+            InitializeComponent();
+            fishFloatMat = CvInvoke.Imread("../piao2.png");
+            timer1.Enabled = true;
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            //注册热键F5，Id号为103。   
+            HotKey.RegisterHotKey(Handle, 103, HotKey.KeyModifiers.None, Keys.O);
+            HotKey.RegisterHotKey(Handle, 104, HotKey.KeyModifiers.None, Keys.P);
+        }
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_HOTKEY = 0x0312;
+            //按快捷键    
+            switch (m.Msg)
+            {
+                case WM_HOTKEY:
+                    switch (m.WParam.ToInt32())
+                    {
+
+                        case 103:
+                            fishStart = true;
+                            break;
+                        case 104:
+                            fishStart = false;
+                            break;
+                    }
+                    break;
+            }
+            base.WndProc(ref m);
+        }
+
+        private Image<Bgr, byte> getScreenImage()
+        {
+            Bitmap memory = new Bitmap(screenW, screenH);
+            Graphics g = Graphics.FromImage(memory);
+            Size mySize = new Size(screenW, screenH);
+            g.CopyFromScreen(0, 0, 0, 0, mySize);
+            return new Image<Bgr, byte>(memory);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Image<Bgr, byte> image = getScreenImage();
+            var floatPoint = getFishFloatPoint(image);
+            CvInvoke.Circle(image, floatPoint, 50, new MCvScalar(255, 0, 0));
+
+        }
+
+        private bool TimerNext = true;
+        private void startFish()
+        {
+            TimerNext = false;
+            keybd_event(Keys.D1, 0, 0, 0);
+            Thread.Sleep(100);
+            keybd_event(Keys.D1, 0, 0x2, 0);
+            richTextBox1.AppendText("key 1 pressed \n" );
+            if (!fishStart)
+            {
+                return;
+            }
+            Thread.Sleep(1500);
+            var floatPoint = getFishFloatPoint();
+            //mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE, floatPoint.X, floatPoint.Y, 0, 0);
+            SetCursorPos(floatPoint.X, floatPoint.Y);
+            richTextBox1.AppendText($"move mouse to X:{floatPoint.X},Y:{floatPoint.Y} \n");
+            if (!fishStart)
+            {
+                return;
+            }
+            Thread.Sleep(1000);
+            mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, floatPoint.X, floatPoint.Y, 0, 0);
+            richTextBox1.AppendText($"mouse right click \n");
+            if (!fishStart)
+            {
+                return;
+            }
+            Thread.Sleep(1000);
+            TimerNext = true;
+
+
+        }
+
+        private Point getFishFloatPoint()
+        {
+            var image = getScreenImage();
+
+            return getFishFloatPoint(image);
+        }
+
+        private Point getFishFloatPoint(Image<Bgr, byte> source)
+        {
+            var mat = new Mat();
+            CvInvoke.MatchTemplate(source, fishFloatMat, mat, TemplateMatchingType.CcoeffNormed);
+            var max_loc = new Point();
+            var min_loc = new Point();
+            double max = 0, min = 0;
+            CvInvoke.MinMaxLoc(mat, ref min, ref max, ref min_loc, ref max_loc);
+            var img2Size = fishFloatMat.Size;
+            int x = (int)(img2Size.Width * 0.5) + max_loc.X;
+            int y = (int)(img2Size.Height * 0.5) + max_loc.Y;
+
+            return new Point(x,y);
+        }
+
+        private void imageBox1_Click(object sender2, EventArgs e2)
+        {
+
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder msgBuilder = new StringBuilder("Performance: ");
+
+            //Load the image from file and resize it for display
+            Image<Bgr, Byte> img =
+               new Image<Bgr, byte>("../300px.png")
+               .Resize(400, 400, Emgu.CV.CvEnum.Inter.Linear, true);
+
+            //Convert the image to grayscale and filter out the noise
+            UMat uimage = new UMat();
+            CvInvoke.CvtColor(img, uimage, ColorConversion.Bgr2Gray);
+
+            //use image pyr to remove noise
+            UMat pyrDown = new UMat();
+            CvInvoke.PyrDown(uimage, pyrDown);
+            CvInvoke.PyrUp(pyrDown, uimage);
+
+            //Image<Gray, Byte> gray = img.Convert<Gray, Byte>().PyrDown().PyrUp();
+
+            #region circle detection
+            Stopwatch watch = Stopwatch.StartNew();
+            double cannyThreshold = 180.0;
+            double circleAccumulatorThreshold = 120;
+            CircleF[] circles = CvInvoke.HoughCircles(uimage, HoughType.Gradient, 2.0, 20.0, cannyThreshold, circleAccumulatorThreshold, 5);
+
+            watch.Stop();
+            msgBuilder.Append(String.Format("Hough circles - {0} ms; ", watch.ElapsedMilliseconds));
+            #endregion
+
+            #region Canny and edge detection
+            watch.Reset(); watch.Start();
+            double cannyThresholdLinking = 120.0;
+            UMat cannyEdges = new UMat();
+            CvInvoke.Canny(uimage, cannyEdges, cannyThreshold, cannyThresholdLinking);
+
+            LineSegment2D[] lines = CvInvoke.HoughLinesP(
+               cannyEdges,
+               1, //Distance resolution in pixel-related units
+               Math.PI / 45.0, //Angle resolution measured in radians.
+               20, //threshold
+               30, //min Line width
+               10); //gap between lines
+
+            watch.Stop();
+            msgBuilder.Append(String.Format("Canny & Hough lines - {0} ms; ", watch.ElapsedMilliseconds));
+            #endregion
+
+            #region Find triangles and rectangles
+            watch.Reset(); watch.Start();
+            List<Triangle2DF> triangleList = new List<Triangle2DF>();
+            List<RotatedRect> boxList = new List<RotatedRect>(); //a box is a rotated rectangle
+
+            using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+            {
+                CvInvoke.FindContours(cannyEdges, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+                int count = contours.Size;
+                for (int i = 0; i < count; i++)
+                {
+                    using (VectorOfPoint contour = contours[i])
+                    using (VectorOfPoint approxContour = new VectorOfPoint())
+                    {
+                        CvInvoke.ApproxPolyDP(contour, approxContour, CvInvoke.ArcLength(contour, true) * 0.05, true);
+                        if (CvInvoke.ContourArea(approxContour, false) > 250) //only consider contours with area greater than 250
+                        {
+                            if (approxContour.Size == 3) //The contour has 3 vertices, it is a triangle
+                            {
+                                Point[] pts = approxContour.ToArray();
+                                triangleList.Add(new Triangle2DF(
+                                   pts[0],
+                                   pts[1],
+                                   pts[2]
+                                   ));
+                            }
+                            else if (approxContour.Size == 4) //The contour has 4 vertices.
+                            {
+                                #region determine if all the angles in the contour are within [80, 100] degree
+                                bool isRectangle = true;
+                                Point[] pts = approxContour.ToArray();
+                                LineSegment2D[] edges = PointCollection.PolyLine(pts, true);
+
+                                for (int j = 0; j < edges.Length; j++)
+                                {
+                                    double angle = Math.Abs(
+                                       edges[(j + 1) % edges.Length].GetExteriorAngleDegree(edges[j]));
+                                    if (angle < 80 || angle > 100)
+                                    {
+                                        isRectangle = false;
+                                        break;
+                                    }
+                                }
+                                #endregion
+
+                                if (isRectangle) boxList.Add(CvInvoke.MinAreaRect(approxContour));
+                            }
+                        }
+                    }
+                }
+            }
+
+            watch.Stop();
+            msgBuilder.Append(String.Format("Triangles & Rectangles - {0} ms; ", watch.ElapsedMilliseconds));
+            #endregion
+
+            //box4.Image = img;
+            this.Text = msgBuilder.ToString();
+
+            #region draw triangles and rectangles
+            Image<Bgr, Byte> triangleRectangleImage = img.CopyBlank();
+            foreach (Triangle2DF triangle in triangleList)
+                triangleRectangleImage.Draw(triangle, new Bgr(Color.DarkBlue), 2);
+            foreach (RotatedRect box in boxList)
+                triangleRectangleImage.Draw(box, new Bgr(Color.DarkOrange), 2);
+            //box1.Image = triangleRectangleImage;
+            #endregion
+
+            #region draw circles
+            Image<Bgr, Byte> circleImage = img.CopyBlank();
+            foreach (CircleF circle in circles)
+                circleImage.Draw(circle, new Bgr(Color.Brown), 2);
+            //box2.Image = circleImage;
+            #endregion
+
+            #region draw lines
+            Image<Bgr, Byte> lineImage = img.CopyBlank();
+            foreach (LineSegment2D line in lines)
+                lineImage.Draw(line, new Bgr(Color.Green), 2);
+            //box4.Image = lineImage;
+            #endregion
+
+            GC.Collect();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (fishStart && TimerNext)
+            {
+                startFish();
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
